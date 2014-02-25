cb137e98d1be5638de46846449c9eff89f99074d
Dawachin <capriccioso.and.grandioso@gmail.com>
2014-02-25 15:26:53 +0900

    ログイン機能実装（userNameとpasswordでDBを検索し該当があるかを検証するメソッドを追加）
    残：
    ログアウト機能の追加
    入力された文字が間違っていた時のエラーメッセージの追加
    passの暗号化（優先度：低）

--- src/main/java/tenkamochi2/action/LoginAction.java
+++ src/main/java/tenkamochi2/action/LoginAction.java
@@ -8,6 +8,8 @@ import org.seasar.struts.annotation.Execute;
 
 import tenkamochi2.dto.UserDto;
 import tenkamochi2.form.UserForm;
+import tenkamochi2.service.TMemberService;
+import tenkamochi2.entity.TMember;
 
 public class LoginAction {
 	
@@ -15,18 +17,27 @@ public class LoginAction {
 	@ActionForm
 	public UserForm userform;
 	
+	/** ログイン情報を格納するDto */
 	@Resource
 	public UserDto userDto;
+	
+	/** TMemberのサービスクラス */
+	@Resource
+	protected TMemberService tMemberService;
 	
 	
 	@Execute(validator = true, input = "/login/index.jsp")
 	public String index(){
-		
-		//セッションへの保存処理
-		//LoginDtoに値を入れるだけでsessionに保存される
-		Beans.copy(this.userform, this.userDto).execute();
-		
-		
+	
+	//入力されたuserNameとpassで、memberテーブルから検索する
+	TMember member = tMemberService.findById_and_pass(userform.userName, userform.password);
+	
+	//検索結果がnullじゃなかったらloginDtoに入力されたuserNameを格納する
+	if (member != null) {
+		        //セッションへの保存処理
+				//LoginDtoに値を入れるだけでsessionに保存される
+				Beans.copy(this.userform, this.userDto).execute();
+	}		
 	return "/mypage/?redirect=true";
 	}
 
